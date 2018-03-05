07c608e28dec2fdfff39dd8a74214853c23b814f
jiangchao08 <jiangchao08@baidu.com>
2018-03-05 11:21:05 +0800

    修正统计时间的bug

---
+++ time_test.cpp
@@ -0,0 +1,9 @@
+//
+//  time_test.cpp
+//  FlightScheduling
+//
+//  Created by Jiang,Chao(AIP) on 18/3/5.
+//  Copyright © 2018年 Jiang,Chao(AIP). All rights reserved.
+//
+
+#include "time_test.hpp"
---
+++ time_test.hpp
@@ -0,0 +1,14 @@
+//
+//  time_test.hpp
+//  FlightScheduling
+//
+//  Created by Jiang,Chao(AIP) on 18/3/5.
+//  Copyright © 2018年 Jiang,Chao(AIP). All rights reserved.
+//
+
+#ifndef time_test_hpp
+#define time_test_hpp
+
+#include <stdio.h>
+
+#endif /* time_test_hpp */
